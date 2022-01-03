93e6de7ed9ca71d7427a0545c060c01ff65f440c
shriom <shriomtripathi33@gmail.com>
2022-01-03 10:37:51 +0530

    Added get, get-one and post order

--- api.js
+++ api.js
@@ -1,13 +1,28 @@
-const cApiBuilder = require('claudia-api-builder');
-const apiBuilder = new cApiBuilder();
 
+const Api = require('claudia-api-builder')
+const api = new Api()
 
-apiBuilder.get('/pizzas', () => {
-  return [
-    'Onion Pizza',
-    'Paneer Pizza',
-    'Peri Peri Pizza'
-  ]
+const getPizzas = require('./handlers/get-pizzas')
+const createOrder = require('./handlers/create-order')
+
+// Define routes
+api.get('/', () => 'Welcome to Pizza API')
+
+api.get('/pizzas', () => {
+  return getPizzas()
 })
 
-module.exports = apiBuilder;
+api.get('/pizzas/{id}', (req) => {
+  return getPizzas(req.pathParams.id)
+}, {
+  error: 404
+})
+
+api.post('/orders', (request) => {
+  return createOrder(request.body)
+}, {
+  success: 201,
+  error: 400
+})
+
+module.exports = api;
