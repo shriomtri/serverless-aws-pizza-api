28d5580d472fc6f53199dbef4c02a144e9a89c3a
shriom <shriomtripathi33@gmail.com>
2022-01-09 18:56:46 +0530

    Feat: create and save  orders to dynamodb

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,10 +1,31 @@
 'use strict'
 
-function createOrder(order) {
-  if (!order || !order.pizzaId || !order.address)
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+const uuid = require('uuid')
+
+function createOrder(payload) {
+  if (!payload || !payload.pizza || !payload.address)
     throw new Error('To order pizza please provide pizza type and address where pizza should be delivered')
 
-  return {}
+  return  docClient.put({
+    TableName: 'pizza-orders',
+    Item: {
+      orderId: uuid.v4() ,
+      pizza: payload.pizza,
+      address: payload.address,
+      status: 'pending'
+    }
+  }).promise()
+    .then((res) => {
+      console.log('Order is saved!', res)
+      return res
+    })
+    .catch((saveError) => {
+      console.log(`Oops, order is not saved :(`, saveError)
+      throw saveError
+    })
 }
 
 module.exports = createOrder
